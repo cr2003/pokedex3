d72685c4abedeb94800b8cf1e1228f368c6d4efb
cr2003 <crodrigo2003@gmail.com>
2016-12-08 11:35:17 +0100

    video 096 final ---> Commit

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Admin on 08/12/2016.
+//  Copyright © 2016 50wise. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
