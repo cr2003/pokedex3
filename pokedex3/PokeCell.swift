72ec7d1219283eb6462816a28191e0e95f8c651a
cr2003 <crodrigo2003@gmail.com>
2016-12-08 12:35:51 +0100

    video 097 final ---> Commit

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Admin on 08/12/2016.
+//  Copyright © 2016 50wise. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
